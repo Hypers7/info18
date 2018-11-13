2bddb4950d84d3c97db1343d7511dcb125fa6e86
hypers7 <fdsaHJKL199233>
2018-11-13 19:04:35 +0800

    抽取配置信息,封装配置文件,定义不同环境下的配置类

---
+++ config.py
@@ -0,0 +1,39 @@
+# 导入redis模块
+from redis import StrictRedis
+
+
+class Config():
+    # 设置debug调试模式
+    DEBUG = None
+    # 配置秘钥
+    SECRET_KEY = '7nqfG7KXkkmg9e3gifTAuQNtXTQlIlYwlRuIibGlXyLjTYNW7MBtWA=='
+
+    # mysql配置信息
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@localhost/python_1'
+    # 动态追踪修改
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 实现状态保持, 配置session信息存储在redis中
+    SESSION_TYPE = 'redis'
+    # 指定redis的主机地址与端口号
+    host = '127.0.0.1'
+    port = 6379
+    SESSION_REDIS = StrictRedis(host, port)
+    SESSION_USE_SIGNER = True
+    # 设置session有效期,flask内置的session有效期配置
+    PERMANENT_SESSION_LIFETIME = 3600
+
+
+# 封装不同环境下的配置类,开发模式与生产模式
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config_dict = {
+    'dev':DevelopmentConfig,
+    'pro':ProductionConfig
+}
--- manage.py
+++ manage.py
@@ -7,28 +7,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
 # 集成状态保持的扩展
 from flask_session import Session
-# 导入redis模块
-from redis import StrictRedis
-
+# 导入配置文件
+from config import config_dict
 app = Flask(__name__)
-# 配置秘钥
-app.config['SECRET_KEY'] = '7nqfG7KXkkmg9e3gifTAuQNtXTQlIlYwlRuIibGlXyLjTYNW7MBtWA=='
-# mysql配置信息
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@localhost/python_1'
-# 动态追踪修改
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-# 实现状态保持, 配置session信息存储在redis中
-app.config['SESSION_TYPE'] = 'redis'
-# 指定redis的主机地址与端口号
-host = '127.0.0.1'
-port = 6379
-app.config['SESSION_REDIS'] = StrictRedis(host, port)
-app.config['SESSION_USE_SIGNER'] = True
-# 设置session有效期,flask内置的session有效期配置
-app.config['PERMANENT_SESSION_LIFETIME'] = 3600
-
+# 使用配置信息
+app.config.from_object(config_dict['dev'])
 
 # 创建sqlalchemy实例对象
 db = SQLAlchemy(app)
