4946472494a1b24b81699a29e94e52b16b373964
hypers7 <fdsaHJKL199233>
2018-11-20 15:40:09 +0800

    新闻列表展示

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,5 +1,5 @@
 # 导入模板对象, 导入上下文对象current_app
-from flask import render_template, current_app, jsonify
+from flask import render_template, current_app, jsonify, request
 # 导入蓝图对象 使用蓝图创建路由映射
 from . import news_blue
 from flask import session
@@ -70,6 +70,73 @@ def index():
         'style': "display: block"
     }
     return render_template('news/index.html', data=data, style=style)
+
+
+@news_blue.route('/news_list')
+def news_list():
+    """
+    首页新闻列表
+    获取参数 检查参数 业务处理 返回接果
+    1 获取参数 get请求 cid page per_page
+    2 检查参数 转换参数的数据类型
+    cid,page,per_page= into(cid),int(page),int(per_page)
+    3 根据分类id查询新闻列表 新闻列表默认安装新闻发布时间倒序排序
+    判断用户选择的新闻分类
+    if cid > 1:
+        News.query.filter(News.category_id == cid).order_by(News.create_time.desc().paginate(page, per_page, false)
+    else:
+        News.query.filter().order_by(News.create_time.desc().paginate(page, per_page, false)
+     filters = []
+    if cid > 1:
+        filters.append(News.category_id == cid)
+    paginate = News.query.filter(*filters).order_by(News.create_time.desc().paginate(page, per_page, false)
+    获取分页后的数据
+    paginate.items
+    paginate.pages
+    paginate.page
+    遍历分页后的数据数据 调用模型类中的方法 转成字典
+    返回结果
+    :return:
+    """
+
+    cid = request.args.get('cid', '1')
+    page = request.args.get('page', '1')
+    per_page = request.args.get('per_page', '10')
+    try:
+        cid,page,per_page = int(cid), int(page), int(per_page)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg='参数格式错误')
+    filters = []
+    if cid > 1:
+        filters.append(News.category_id == cid)
+    try:
+        paginate = News.query.filter(*filters).order_by(News.create_time.desc().paginate(page, per_page, False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg='查询新闻列表失败')
+    # 使用分页对象 获取分页后的数据,总数据,总页数,当前页数
+    news_list = paginate.items
+    total_page = paginate.pages
+    current_page = paginate.page
+    news_dict_list = []
+    # 遍历分页数据 转成字典
+    for news in news_list:
+        news_dict_list.append(news.to_dict())
+    # 定义容器
+    data = {
+        'total_page': total_page,
+        'current_page': current_page,
+        'news_dict_list': news_dict_list
+    }
+
+    # 返回数据
+    return jsonify(errno=RET.OK, errmsg='ok', data=data)
+
+
+
+
+
 
 
 # 加载logo图标:浏览器会默认请求 url地址:http://127.0.0.1:5000/favicon.ico
